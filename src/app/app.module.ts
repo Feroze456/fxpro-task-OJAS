62c02924cc2ae61f61d0b8a5dd4bf7010b5e015e
Feroze456 <43064170+Feroze456@users.noreply.github.com>
2020-10-06 10:51:34 +0530

    FX-PRO-TASK

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Home2Component } from './home2/home2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    Home2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
